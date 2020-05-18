d950cf6daa6632c5fbd5ada35a3eaed03fbbda30
Ignacio Donovan <ignacio.donovan@xero.com>
2020-05-19 00:18:14 +1200

    AppText styles for Platform specific

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, Platform } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import AppText from './app/components/AppText'
 
 export default function App() {
   // return <WelcomeScreen />
-  return <ViewImageScreen />
+  // return <ViewImageScreen />
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center"
+    }}
+    >
+      <AppText>
+        I love React Native! this is my first react text. Here is some more text</AppText>
+      <MaterialCommunityIcons name='email' size={60} />
+    </View>
+  );
 }
