38bb4f71cd142297af0c0eff9ae46219d97e8d0f
Ignacio Donovan <ignacio.donovan@xero.com>
2020-05-19 20:35:34 +1200

    adding TouchableWithoutFeedback

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
 
 import ListItem from '../components/ListItem';
@@ -6,7 +6,7 @@ import Screen from '../components/Screen';
 import ListItemSeparator from '../components/ListItemSeparator';
 import ListItemDeleteAction from '../components/ListItemDeleteAction';
 
-const messages = [
+const initialMessages = [
     {
         id: 1,
         title: 'T1',
@@ -58,6 +58,15 @@ const messages = [
 ]
 
 function MessagesScreen(props) {
+    const [messages, setMessages] = useState(initialMessages); // return an array with count and setCount function
+
+    const handleDelete = message => {
+        // Delete teh message from messages
+        const newMessages = messages.filter(m => m.id !== message.id)
+        setMessages(newMessages)
+        // TODO Call the server
+    }
+
     return (
         <Screen>
             <FlatList
@@ -69,7 +78,9 @@ function MessagesScreen(props) {
                         description={item.description}
                         image={item.image}
                         onPress={() => console.log("Message selected", item)}
-                        renderRightActions={ListItemDeleteAction}
+                        renderRightActions={() =>
+                            <ListItemDeleteAction onPress={() => handleDelete(item)} />
+                        }
                     />
                 }
                 ItemSeparatorComponent={ListItemSeparator}
