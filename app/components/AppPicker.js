e1f0948da70fed10e704827779b64e6336df2163
Ignacio Donovan <ignacio.donovan@xero.com>
2020-05-21 00:01:33 +1200

    Fix Picker text color

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -20,7 +20,11 @@ function AppPicker({ icon, items, onSelectItem, placeholder, selectedItem }) {
                             size={20}
                             color={defaultStyles.colors.medium}
                             style={styles.icon} />}
-                    <AppText style={styles.text}>{selectedItem ? selectedItem.label : placeholder}</AppText>
+                    {selectedItem ? (
+                        <AppText style={styles.text}>{selectedItem.label}</AppText>
+                    ) : (
+                            <AppText style={styles.placeholder}>{placeholder}</AppText>
+                        )}
                     <MaterialCommunityIcons
                         name="chevron-down"
                         size={20}
@@ -63,8 +67,13 @@ const styles = StyleSheet.create({
     icon: {
         marginRight: 10
     },
+    placeholder: {
+        flex: 1,
+        color: defaultStyles.colors.medium
+
+    },
     text: {
-        flex: 1
+        flex: 1,
     }
 })
 export default AppPicker;
